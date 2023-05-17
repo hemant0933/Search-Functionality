8d76ba7a18fc4c96b27bf339eca8a0c3878a9890
hemant0933 <116268777+hemant0933@users.noreply.github.com>
2023-05-17 11:04:48 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState} from 'react';
+import JSONDATA from '../src/MOCK_DATA.json';
 
 function App() {
+
+  const [search,setSearch] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+     <input type='text' value={search} placeholder='Search....' onChange={(e) => setSearch(e.target.value)} />
+     <div className='cardContainer'>
+      {
+        JSONDATA.filter((item) => {
+          return search.toLowerCase() === '' ? item
+          : item.first_name.toLowerCase().includes(search.toLowerCase());
+        }).map((item,index) => (
+          <div key={index} className='cards'>
+            <div className='ImgBg' style={{background:'black',width:'90px',height:'70px',borderRadius:'100px'}}><img src={item.Avatar} alt="avatar"/></div><br/>
+            <p>Name : {item.first_name} {item.last_name}</p>
+            <p>Email : {item.email}</p>
+            <p>Gender : {item.gender}</p>
+          </div>
+        ))
+      }
+     </div>
+
     </div>
   );
 }
